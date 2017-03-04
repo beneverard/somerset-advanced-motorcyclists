d572048bc8d380fdd258d419077caeca6689d599
Ben Everard <ben@theideabureau.co>
2017-03-04 17:54:58 +0000

    Added typekit script

--- header.php
+++ header.php
@@ -28,6 +28,21 @@
 		<!-- <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/assets/img/favicons/apple-touch-icon-144x144.png" /> -->
 		<!-- <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/assets/img/favicons/apple-touch-icon-152x152.png" /> -->
 
+		<script>
+
+			(function(d) {
+
+				var config = {
+					kitId: 'wly2gjg',
+					scriptTimeout: 3000,
+					async: true
+				},
+				h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+				
+			})(document);
+
+		</script>
+
 		<?php wp_head(); ?>
 
 	</head>
