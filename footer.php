fe4e0be9f121907a96dca51de058eaf787f2d4a0
Ben Everard <ben@theideabureau.co>
2017-04-29 11:02:49 +0100

    Added footer styles

--- footer.php
+++ footer.php
@@ -1,6 +1,15 @@
 <?php // footer.php ?>
 
-			<footer>
+			<footer class="site-footer">
+
+				<a href="/" class="site-footer__logo">
+			  	  <img src="/wp-content/themes/sam/public/images/logo.svg" />
+			    </a>
+
+				<div class="site-footer__colophon">
+					<p>Copyright <?php echo date('Y'); ?> Somerset Advanced Motorcyclists</p>
+					<p>Supported by <a href="https://theideabureau.co">The Idea Bureau</a></p>
+				</div>
 
 			</footer>
 
